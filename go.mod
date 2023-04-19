46d19b530db3a2ee41d49fd12b76599e27d480c0
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-04-19 16:43:27 +0100

    Simple Server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ffranciscofferraz/product-api
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/ffranciscofferraz/product-api/handlers"
+)
+
+func main() {
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	HelloHandler := handlers.NewHello(l)
+	goodbyeHandler := handlers.NewGoodbye(l)
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/hello", HelloHandler)
+	serveMux.Handle("/goodbye", goodbyeHandler)
+
+	server := &http.Server{
+		Addr:         ":9090",
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	sig := <-signalChannel
+	l.Println("Received terminate, shutdown", sig)
+
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(timeoutContext)
+}
