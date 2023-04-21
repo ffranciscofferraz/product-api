066457cec1c4d572126fdef92a71cc4caeb2b114
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-04-21 21:41:09 +0200

    Added Tests

--- api/sdk/main_test.go
+++ api/sdk/main_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/franciscofferraz/coffee-shop/api/sdk/client"
+	"github.com/franciscofferraz/coffee-shop/api/sdk/client/products"
+)
 
 func TestClient(t *testing.T) {
+	cfg := client.DefaultTransportConfig().WithHost("localhost:9090")
+	client := client.NewHTTPClientWithConfig(nil, cfg)
+	params := products.NewListProductsParams()
+
+	product, err := client.Products.ListProducts(params)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(product)
 
 }
