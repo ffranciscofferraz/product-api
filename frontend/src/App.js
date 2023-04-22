b14ec16629feab897c74fbb10ca76c5feee63030
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-04-22 16:56:00 +0200

    HTTP multi-part requests and Simple Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+import './global'
+
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import CoffeeList from './CoffeeList.js'
+import Admin from './Admin.js'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Coffee Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/admin">Admin</Nav.Link>
+            </Nav>
+            <Form inline="true">
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={<CoffeeList />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
